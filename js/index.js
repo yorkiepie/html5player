405116d0ec8f6a0968bd8a42ecb699ef654d7371
Puneet Johar <puneet.johar@piksel.com>
2016-09-16 15:22:49 +0100

    initial commit

---
+++ js/index.js
@@ -0,0 +1,80 @@
+$('.toggle, .inner-toggle').click(function(e) {
+  	e.preventDefault();
+    var $this = $(this);
+    if ($this.next()[0].classList.contains('show')) {
+        $this.next().removeClass('show');
+        $this.next().slideUp(350);
+    } else {
+        $this.parent().parent().find('li .inner, li .first').removeClass('show');
+        $this.parent().parent().find('li .inner, li .first').slideUp(350);
+        $this.next().toggleClass('show');
+        $this.next().slideToggle(350);
+    }
+});
+
+var myvideo = document.getElementById('myvideo'),
+    playbutton = document.getElementById('playme'),
+    restart = document.getElementById('restart'),
+    chapt1 = document.getElementById('chapt1'),
+    chapt2 = document.getElementById('chapt2'),
+    chapt3 = document.getElementById('chapt3');
+
+chapt1.addEventListener("click", function (event) {
+    event.preventDefault();
+    myvideo.play();
+    myvideo.pause();
+    myvideo.play();
+  	myvideo.src = "http://cdn-origin-discovery.digitalshowcase.piksel.com/trailers/2016/09/15/10/35/1b_RM_CO3_HDR_Clip1_22M.mp4";
+    myvideo.autoplay = true;
+  	myvideo.currentTime = 0;
+}, false);
+
+chapt2.addEventListener("click", function (event) {
+    event.preventDefault();
+    myvideo.src="http://cdn-origin-discovery.digitalshowcase.piksel.com/trailers/2016/09/16/08/34/1a_RM_SDR_Clip1_22M.mp4"
+    myvideo.currentTime = 0;
+    myvideo.play();
+}, false);
+
+chapt3.addEventListener("click", function (event) {
+    event.preventDefault();
+    myvideo.src="http://cdn-origin-discovery.digitalshowcase.piksel.com/trailers/2016/09/16/13/45/1a_RM_SDR_Clip2_22M.mp4";
+    myvideo.currentTime = 0;
+    myvideo.play();
+}, false);
+
+// only in to demonstrate video
+playbutton.addEventListener("click", function () {
+    if (myvideo.paused) {
+        myvideo.play();
+    } else {
+        myvideo.pause();
+    }
+}, false);
+
+restart.addEventListener("click", function (event) {
+    event.preventDefault();
+    myvideo.play();
+    myvideo.pause();
+    myvideo.currentTime = 0;
+    myvideo.play();
+}, false);
+
+
+$(document).ready(function() {
+    var audioElement = document.createElement('audio');
+    audioElement.setAttribute('src', 'http://www.freesfx.co.uk/rx2/mp3s/2/2706_1329133089.mp3');
+   
+  $.get();
+    audioElement.addEventListener("load", function() {
+      audioElement.play();
+    }, true);
+  
+    $('.toggle').click(function() {
+      audioElement.play();
+    });
+  
+    $('.inner-toggle').click(function() {
+      audioElement.play();
+    });
+});
